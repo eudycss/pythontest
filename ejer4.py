28f9ad14c7fc903af37d0b91f668a75b774f8e70
Sebas CS <euddyk@gmail.com>
2022-03-23 11:47:18 -0500

    first commit

---
+++ ejer4.py
@@ -0,0 +1,8 @@
+nombre=input("Ingrese su nombre")
+apellido=input("Ingrese su Apellido")
+fullName=nombre+" "+apellido
+anio=int(input("Ingrese su año nacimiento: "))
+edad =2022-anio
+print("Nombre completo es:",fullName)
+
+print("Edad",edad)
